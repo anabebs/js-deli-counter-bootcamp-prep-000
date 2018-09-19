2b4d15bfd2df9d5969a892860445c36d6757d184
Ana Bebic <anabebic.x@gmail.com>
2018-09-19 10:03:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ function nowServing(katzDeliLine){
 
       }
       
+function currentLine(katzDeliLine){
+  var str = `The line is currently: `;
+  var newLine = [];
+  if (katzDeliLine.length === 0) return "The line is currently empty.";
+  else{
+    for (let i = 0; i < katzDeliLine.length; i++){
+        newLine[i] = `${[i+1]}. ${katzDeliLine[i]}`;
+      }
+    str += newLine.join(', ')
+    return str;
+  }
+}
