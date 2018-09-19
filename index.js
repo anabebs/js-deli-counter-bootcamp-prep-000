b04ff8e9bd0cc650ebf24383f7b870ae75bf034a
Ana Bebic <anabebic.x@gmail.com>
2018-09-19 09:57:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ return `Welcome, ${name}, you are number katzDeliLine.length in line`)
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length > 0) return `Now serving {katzDeliLine.shift()}.`
-  
+  else return "There is nobody waiting to be served!";
+
   
 }
