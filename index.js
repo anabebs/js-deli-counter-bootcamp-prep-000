7355409a12c543c28c14c983183e2c70722ea146
Ana Bebic <anabebic.x@gmail.com>
2018-09-19 09:56:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ console.log("Welcome, ${name}, you are number katzDeliLine.length in line")
 };
 
 function nowServing(katzDeliLine){
-  if (katzDeliLine.length > 0) return "Now serving katzDeliLine.shift"
+  if (katzDeliLine.length > 0) return "Now serving katzDeliLine.shift()"
   
   
 }
